65a17d486a0d106780f5a589ae7d711140f4b95d
Douglas Tiepolo <dvtiepolo@gmail.com>
2020-05-02 22:04:45 +0100

    update

---
+++ project/src/js/themeSwitcher.js
@@ -0,0 +1,26 @@
+let lightMode = localStorage.getItem("lightMode");
+const lightModeSwitcher = document.querySelector('#light-mode-switcher');
+
+const enableLightMode = () => {
+    document.body.classList.add("lightMode");
+    localStorage.setItem("lightMode", "enabled");
+};
+
+const disableLightMode = () => {
+    document.body.classList.remove("lightMode");
+    localStorage.setItem("lightMode", null);
+};
+
+if (lightMode === "enabled") {
+    enableLightMode();
+}
+
+lightModeSwitcher.addEventListener("click", () => {
+    lightMode = localStorage.getItem("lightMode");
+    if (lightMode !== "enabled") {
+        enableLightMode();
+    } else {
+        disableLightMode();
+    }
+});
+
